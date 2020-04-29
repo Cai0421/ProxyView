ae9fb519f7915dde02605bf7370b45a10b98b3a9
Cai0421 <Cai0922421@outlook.com>
2020-04-29 12:44:53 +0800

    倒计时完成

--- ProxyView/MainWindow.xaml.cs
+++ ProxyView/MainWindow.xaml.cs
@@ -14,7 +14,9 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Forms;
+using System.Windows.Threading;
 using ProxyView.DataModel;
+using ProxyView.Model;
 namespace ProxyView
 {
     /// <summary>
@@ -25,12 +27,15 @@ namespace ProxyView
         WindowState ws;
         WindowState wsl;
         NotifyIcon notifyIcon;
+        private DispatcherTimer timer;
+        private ProcessCount processCount;
         public MainWindow()
         {
             InitializeComponent();
             this.DataContext = new MainWindowViewModel();
             icon();
             wsl = WindowState;
+            this.Loaded += new RoutedEventHandler(MainWin_Loaded);
         }
 
         //icon设置
@@ -48,7 +53,52 @@ namespace ProxyView
             this.Show();
             WindowState = wsl;
         }
-        
+        //窗口加载部分
+        private void MainWin_Loaded(object sender, RoutedEventArgs e)
+        {
+            //设置定时器
+            
+            timer = new DispatcherTimer();
+            timer.Interval = new TimeSpan(10000000);   //时间间隔为一秒
+            timer.Tick += new EventHandler(timer_Tick);
+
+            //转换成秒数
+            Int32 hour = Convert.ToInt32(Hour_TextBox.Text);
+            Int32 minute = Convert.ToInt32(Miniute_TextBox.Text);
+            Int32 second = Convert.ToInt32(Second_TextBox.Text);
+
+            //处理倒计时的类
+            processCount = new ProcessCount(hour * 3600 + minute * 60 + second);
+            CountDown += new CountDownHandler(processCount.ProcessCountDown);
+
+            //开启定时器
+            timer.Start();
+        }
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if (OnCountDown())
+            {
+                Hour_TextBox.Text = processCount.GetHour();
+                Miniute_TextBox.Text = processCount.GetMinute();
+                Second_TextBox.Text = processCount.GetSecond();
+            }
+            else
+                timer.Stop();
+        }
+
+        /// <summary>
+        /// 处理事件
+        /// </summary>
+        public event CountDownHandler CountDown;
+        public bool OnCountDown()
+        {
+            if (CountDown != null)
+                return CountDown();
+
+            return false;
+        }
+        public delegate bool CountDownHandler();
+
         private void NavBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             DragMove();
